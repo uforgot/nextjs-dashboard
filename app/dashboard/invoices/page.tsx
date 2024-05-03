0871e39ccd5dd4875b947cc8b4da6543b77d7077
uforgot <uforgot@me.com>
2024-05-03 14:08:52 +0900

    [add] add meta data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,11 @@ import Table from '@/app/ui/invoices/table';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import Pagination from '@/app/ui/invoices/pagination';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page({
   searchParams,
 }: {
